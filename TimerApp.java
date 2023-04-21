db1bbcf5a000cfe5621e9aca958645ed92c9aa3d
Jun <kumejun2000@gmail.com>
2023-04-21 13:14:37 +0900

    インスタンス変数とメソッドの追加

---
+++ TimerApp.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/**
+ * TimerApp
+ */
+public class TimerApp extends JFrame implements ActionListener {
+    private JTextField timeField;
+    private JButton starButton, pauseButton, resetButton;
+    private JLabel timeLabel;
+
+    //コンストラクタ(Pythonで言うと__init__)
+    public TimerApp() {
+        ;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        ;
+    }
+
+    public static void main(String[] args) {
+        ;
+    }
+    
+}
